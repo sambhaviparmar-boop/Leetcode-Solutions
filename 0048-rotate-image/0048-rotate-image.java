0a8751d4248e94a907aa8240b5e0086462350082
Sambhavi Parmar <sambhaviparmar@gmail.com>
2026-06-18 08:44:58 +0530

    Time: 0 ms (100%), Space: 44 MB (15.71%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void rotate(int[][] arr) {
+      for(int i=0; i<arr.length; i++){
+        for(int j=i+1; j<arr.length; j++){
+            int temp = arr[i][j];
+            arr[i][j]=arr[j][i];
+            arr[j][i]=temp;
+        }
+      }
+       for(int i=0; i<arr.length; i++){
+       int left=0;
+       int right = arr.length-1;
+       while(left<right){
+       
+   
+                int temp = arr[i][left];
+                arr[i][left]= arr[i][right];
+                arr[i][right] = temp;
+                left++;
+                right--;
+            }
+        
+
+       }
+    }
+}
