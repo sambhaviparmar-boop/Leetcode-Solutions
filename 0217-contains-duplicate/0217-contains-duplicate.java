eb3c42b28a4c768228162ef7a3d2a819ebf0ae08
Sambhavi Parmar <sambhaviparmar@gmail.com>
2026-07-14 12:35:57 +0530

    Time: 18 ms (47.95%), Space: 108.4 MB (9.41%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+           
+      
+            if(set.contains(num)){
+                return true;
+            }else{
+                set.add(num);
+            }
+        }
+        return false;
+    }
+}
