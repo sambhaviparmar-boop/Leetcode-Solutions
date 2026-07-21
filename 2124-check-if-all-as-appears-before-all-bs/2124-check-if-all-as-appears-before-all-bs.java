93bdb3708e15c84e663122af478b5a0ffdeaf83d
Sambhavi Parmar <sambhaviparmar@gmail.com>
2026-07-21 22:27:32 +0530

    Time: 0 ms (100%), Space: 42.9 MB (41.07%) - LeetHub

---
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkString(String s) {
+        boolean b = false;
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == 'b' && s.charAt(i + 1) == 'a') {
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
