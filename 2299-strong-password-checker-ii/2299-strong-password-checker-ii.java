1e64a680f634d6257b13ec33584b4907b60ab70f
Sambhavi Parmar <sambhaviparmar@gmail.com>
2026-07-25 22:30:21 +0530

    Time: 1 ms (78.15%), Space: 42.6 MB (81.01%) - LeetHub

--- 2299-strong-password-checker-ii/2299-strong-password-checker-ii.java
+++ 2299-strong-password-checker-ii/2299-strong-password-checker-ii.java
@@ -1,40 +1,34 @@
 class Solution {
     public boolean strongPasswordCheckerII(String password) {
+      boolean hasLower = false;
+      boolean hasUpper = false;
+      boolean hasDigit = false;
+      boolean hasSpecial = false;
 
-        boolean hasLower = false;
-        boolean hasUpper = false;
-        boolean hasDigit = false;
-        boolean hasSpecial = false;
+     if(password.length()<8){
+            return false;
+        }
+
+     for(int i=0; i<password.length(); i++){
+         char ch = password.charAt(i);
+
+
+        
+         if(i <  password.length()-1){
+            if(password.charAt(i) == password.charAt(i+1)) return false;
+        }
        
+ 
+        if(Character.isLowerCase(ch)) hasLower = true;
+        if(Character.isUpperCase(ch)) hasUpper = true;
+        if(Character.isDigit(ch)) hasDigit = true;
+        if(!Character.isLetterOrDigit(ch)) hasSpecial = true;
 
-        for (int i = 0; i < password.length(); i++) {
-            char ch = password.charAt(i);
+      
 
-            if (password.length() < 8) {
-                return false;
-            }
-            if (Character.isLowerCase(ch)) {
-                hasLower = true;
-            }
-            if (Character.isUpperCase(ch)) {
-                hasUpper = true;
-            }
-            if (Character.isDigit(ch)) {
-                hasDigit = true;
-            }
-            if (!Character.isLetterOrDigit(ch)) {
-                hasSpecial = true;
-            }
-            if(i < password.length()-1){
-                if (password.charAt(i) == password.charAt(i + 1)) {
-               return false;
-            }
-            }
+      
 
-        }
-        if (hasLower && hasUpper && hasDigit && hasSpecial) {
-            return true;
-        }
-        return false;
+     }
+      return hasLower && hasUpper && hasDigit && hasSpecial;
     }
 }
