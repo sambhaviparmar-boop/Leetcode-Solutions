55e88822043a33e3861eb96cf05a7b983c82471a
Sambhavi Parmar <sambhaviparmar@gmail.com>
2026-07-27 18:50:44 +0530

    Time: 520 ms (6.19%), Space: 47.8 MB (26.83%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.java
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.java
@@ -1,24 +1,27 @@
 class Solution {
     public int mostFrequentEven(int[] nums) {
-       int maxFreq = 0;
-       int ans = -1;
-        for(int i=0; i<nums.length ; i++){
-             int count = 0;
-            for(int j = 0; j < nums.length; j++){
-            if(nums[i]%2 ==0){
-                if(nums[i]==nums[j]){
-                   count++;
+      int maxFreq = 0;
+      int ans = -1;
+
+      for(int i=0 ; i<nums.length; i++){
+        int count = 0;
+
+        for(int j=0 ; j<nums.length ; j++){
+            if(nums[i] %2 ==0){
+                if(nums[i] == nums[j]){
+                    count++;
                 }
-            } 
-            }
-            if(count>maxFreq){
-                maxFreq = count; 
-                ans = nums[i];
-            }
-            else if(count == maxFreq && nums[i]<ans){
-                 ans = nums[i];
             }
         }
-        return ans;
+
+        if(maxFreq<count){
+            maxFreq = count;
+            ans = nums[i];
+        }
+        else if(maxFreq == count && ans>nums[i]){
+            ans = nums[i];
+        }
+      }
+      return ans;
     }
 }
