3b16cbc7e3d46a3da3d413b7d37c69bc4c4bcffd
Sambhavi Parmar <sambhaviparmar@gmail.com>
2026-08-11 22:00:51 +0530

    Time: 2 ms (99.31%), Space: 43.5 MB (42.25%) - LeetHub

--- 0804-unique-morse-code-words/0804-unique-morse-code-words.java
+++ 0804-unique-morse-code-words/0804-unique-morse-code-words.java
@@ -3,17 +3,18 @@ class Solution {
         String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
         HashSet<String> set = new HashSet<>();
 
-        for(String word: words ){
-        StringBuilder sb = new StringBuilder();
-             
+        for(String word: words){
+            StringBuilder sb = new StringBuilder();
 
-             for(int i=0; i<word.length(); i++){
-                char ch = word.charAt(i);
-               sb.append( morse[ch - 'a']);
-           }
-           set.add(sb.toString());
-             }
+            for(int i=0; i<word.length(); i++){
+            char ch = word.charAt(i);
 
-        return set.size();
+            sb.append(morse[ch-'a']);
+           
+        }
+         set.add(sb.toString());
+        }
+       return  set.size();
     }
 }
+
