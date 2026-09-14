632df7f606052e4f52d0c67ee1102bf0c1f25f95
Sambhavi Parmar <sambhaviparmar@gmail.com>
2026-09-14 12:01:45 +0530

    Time: 0 ms (100%), Space: 47.7 MB (77.32%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,21 +1,22 @@
 class Solution {
     public double myPow(double x, int n) {
         
-        long binaryForm = n;
-        double ans = 1;
+      long binForm = n;
+      double ans = 1;
 
-        if(n < 0){
-            x = 1/x;
-            binaryForm = -binaryForm;
-        }
+      if(n < 0){
+        x = 1/x;
+        binForm = -binForm;
+      }
 
-        while(binaryForm > 0){
-            if(binaryForm % 2 == 1){
-                ans = ans * x;
-            }
-            x = x * x;
-            binaryForm = binaryForm / 2;
+      while(binForm > 0){
+
+        if(binForm % 2 == 1){
+            ans = ans * x;
         }
-        return ans ;
+        x = x * x;
+        binForm = binForm / 2;
+      }
+      return ans;
     }
 }
