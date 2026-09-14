36458c689553ce0d27f5cd1070edfb49f8e8d65f
Sambhavi Parmar <sambhaviparmar@gmail.com>
2026-09-14 17:06:08 +0530

    Time: 0 ms (100%), Space: 44.6 MB (54.95%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,29 +1,32 @@
 class Solution {
-   void paranthesis(int n , int left , int right ,List<String> ans , StringBuilder temp){
+   void paranthesis(int n , int l , int r, ArrayList<String> ans, StringBuilder temp){
 
-    if(left + right == 2 * n){
+      if(l + r == 2 * n){
         ans.add(temp.toString());
         return;
-    }
-    if(left < n){
-        temp.append("(");
-        paranthesis(n, left+1, right, ans, temp);
+      }
+
+      if(l < n){
+       temp.append("(");
+        paranthesis(n , l+1,r,  ans, temp);
         temp.deleteCharAt(temp.length()-1);
-    }
-    if(right < left){
-       temp.append(")");
-       paranthesis(n, left, right+1, ans , temp);
-       temp.deleteCharAt(temp.length()-1);
-    }
+      }
+      if(r < l){
+        temp.append(")");
+        paranthesis(n, l ,r+1, ans , temp);
+        temp.deleteCharAt(temp.length()-1);
+      }
    }
 
 
 
 
     public List<String> generateParenthesis(int n) {
-        List<String> ans = new ArrayList<>();
-        StringBuilder temp = new StringBuilder();
-        paranthesis(n, 0 , 0, ans , temp);
-        return ans;
+       ArrayList<String> ans = new ArrayList<>();
+       StringBuilder temp = new StringBuilder();
+
+       paranthesis(n , 0, 0, ans , temp);
+
+       return ans;
     }
 }
