1c3c0e4484174d28503e47e1aad5a9245ad09e49
Sambhavi Parmar <sambhaviparmar@gmail.com>
2026-09-17 22:17:38 +0530

    Time: 0 ms (100%), Space: 42.2 MB (16.14%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,9 +1,22 @@
 class Solution {
     public int fib(int n) {
-        if(n <= 1){
+
+        int[] hoo = new int [n+1];
+        
+        if(n == 1 || n==0){
             return n;
         }
 
-        return fib(n-1) + fib(n-2);
+     
+        hoo[1] = 1;
+
+       for(int i = 2; i< hoo.length ; i++){
+
+       hoo[i] = hoo[i-1] + hoo[i-2];
+         
+       }
+
+ return hoo[n];
+        
     }
 }
