52837881263c9c3ae6d5f133f68717790c42b53f
Sambhavi Parmar <sambhaviparmar@gmail.com>
2026-09-22 12:36:35 +0530

    Time: 0 ms (100%), Space: 42.2 MB (19.68%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,13 +1,22 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        
-    
+        long s = 1;
+        long e = num;
 
-      for(long i = 1; i* i <= num ; i++){
-        if( i * i == num){
-            return true;
+        while(s <= e){
+            long mid = s + (e-s)/2;
+            long sq = mid * mid ;
+
+            if(sq == num){
+                return true;
+            }
+            else if (sq < num){
+               s = mid+1;
+            }
+            else{
+                e = mid-1;
+            }
         }
-      }
-      return false;
+        return false;
     }
 }
